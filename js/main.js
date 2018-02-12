7f441905e7532e517c0dbad65e79cdab50d6055a
Scott <scotthuston1982@gmail.com>
2018-02-12 13:50:10 -0500

    finished

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+var light = document.querySelector('.switch');
+var notstatus = document.querySelector('.status');
+var body = document.querySelector('.light');
 
 
+
+light.addEventListener('click', function(){
+  body.classList.toggle('light');
+  body.classList.toggle('dark');
+  light.classList.toggle('on');
+  light.classList.toggle('off');
+  if(body.classList === 'light'){
+    notstatus.innerText = "It's so bright in here!";
+  } else {
+    notstatus.innerText = "who turned out the lights?";
+  }
+});
